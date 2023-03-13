6e0c65eaa89ff6484e7b3135b0a7336d28d9ca53
Hanbyul Lee <hanbyul.lee@dramancompany.com>
2023-03-13 13:26:57 +0900

    ssm 으로 관리되고 있는(associated) 인스턴스만 추리고, ansible 에서 사용할 수 있는 형태로 콘솔에 출력하도록 하였음

--- ssm_agent/list_up.rb
+++ ssm_agent/list_up.rb
@@ -1,4 +1,5 @@
 require 'aws-sdk-ec2'
+require 'aws-sdk-ssm'
 
 module SsmAgent
   class ListUp
@@ -20,14 +21,47 @@ module SsmAgent
         response.map(&:data)
                 .group_by { |i| i[:key_name] }
                 .each do |ssh_key_name, instances|
-          puts "[[[[ ssh_key_name : #{ssh_key_name} ]]]]"
-          instances.filter { |i| i.state.name == 'running' }
-            .each do |instance_data|
-            puts "#{ssh_key_name}, #{instance_data.instance_id}, #{instance_data.private_ip_address},  #{instance_data.state}, #{instance_data.platform}, #{instance_data.architecture}"
+          puts "[[[[ ssh_key_name : #{ssh_key_name}, count: #{instances.size} ]]]]"
+          puts "[#{ssh_key_name}]"
+          instances_running_without_ssm_agent = instances.filter { |i| i.state.name == 'running' }
+                         .filter do |i|
+            sleep(0.2) # Rate exceeded 오류 방지
+            !ssm_enabled?(i.instance_id)
           end
+          puts "instances_running_without_ssm_agent.size: #{instances_running_without_ssm_agent.size}"
+          instances_running_without_ssm_agent
+                   .each_with_index do |instance_data, idx|
+            # puts "#{idx}, #{ssh_key_name}, #{instance_data.instance_id}, #{instance_data.private_ip_address},  #{instance_data.state.name}, #{instance_data.platform}, #{instance_data.architecture}"
+            puts "#{instance_data.private_ip_address}"
+          end
+          puts
+          puts("[#{ssh_key_name}:vars]")
+          puts("ansible_ssh_user=ec2-user")
+          puts("ansible_ssh_private_key_file=/home/ec2-user/.ssh/#{ssh_key_name}.pem")
+          puts
         end
       rescue StandardError => e
         puts "Error getting information about instances: #{e.message}"
+      end
+
+      def ssm_enabled?(ec2_id)
+        @client ||= Aws::SSM::Client.new
+        response = @client.describe_instance_information({
+                                                           instance_information_filter_list: [
+                                                             {
+                                                               key: 'InstanceIds',
+                                                               value_set: [ec2_id]
+                                                             }
+                                                           ],
+                                                           max_results: 5
+                                                         })
+        return false unless response.next_token.nil?
+        return false if response.first.nil?
+        return false if response.first.instance_information_list.nil?
+        return false if response.first.instance_information_list[0].nil?
+
+        var = response.first.instance_information_list[0][:association_status]
+        var == 'Success'
       end
     end
   end
